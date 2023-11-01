870c12b47639b3e2f252566a4e7cac9cdb238fba
maitrypatel1999 <maitrypatel1999@gmail.com>
2023-11-01 15:28:07 -0400

    create a repo

---
+++ src/test/java/job/interview/practice/program/ProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package job.interview.practice.program;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
